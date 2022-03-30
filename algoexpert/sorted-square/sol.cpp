b967f6f29279f1ba8522bbc268547df8efafa291
Nowfel Mashnoor <nmmashnoor@gmail.com>
2022-03-30 17:32:21 +0600

    solved soted square

--- algoexpert/sorted-square/sol.cpp
+++ algoexpert/sorted-square/sol.cpp
@@ -1,20 +1,61 @@
 #include <vector>
 #include <stack>
+#include <cmath>
+#include <iostream>
 using namespace std;
 
 vector<int> sortedSquaredArray(vector<int> array) {
-  vector<int> v;
-  for(int i = 0; i<array.size(); i++)
+  
+ 
+  vector<int> res;
+  int j = -1;
+	
+  for(int i = 0; i<array.size();)
   {
-      v.push_back(array[i] * array[i]);
+   
+		 if(array[i] < 0)
+     {
+       j = i;
+       i++;
+     }
+     else
+     {
+       if(j>=0 && array[i] > abs(array[j]))
+       {
+        
+         res.push_back(array[j] * array[j]);
+         j--;
+
+       }
+       else
+       {
+         res.push_back(array[i]*array[i]);
+         i++;
+       }
+     }
+
+      
   }
-  return v;
+  while(j >= 0)
+  {
+    res.push_back(array[j] * array[j]);
+    j--;
+  }
+
+  return res;
+
+
 }
 
 
 int main()
 {
-
+    vector<int> v = { -10, -5, 0, 5, 10 };
+    vector<int> res = sortedSquaredArray(v);
+    for(int i = 0; i<res.size(); i++)
+    {
+      cout<<res[i]<<endl;
+    }
 
     return 0;
 }
