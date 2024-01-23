6ff3553bcde9563a7bb88d5f4ae7647c6f4ccb58
Nowfel Mashnoor <nmmashnoor@gmail.com>
2024-01-23 13:51:40 -0800

    Time: 264 ms (21.61%), Space: 87.4 MB (25.88%) - LeetHub

---
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool not_overlap(string& s1, string& s2)
+    {
+        int mp[26] = {0};
+        for(auto& c : s1)
+        {
+            int v = c - 'a';
+            mp[v]++;
+        }
+        for(auto& c : s2)
+        {
+            int v = c - 'a';
+            mp[v]++;
+        }
+        for(auto& v: mp)
+        {
+            if(v > 1) return false;
+        }
+        
+        return true;
+    }
+    
+    int bt(vector<string>& arr, int i, string  s)
+    {
+        if(i == arr.size()) return s.length();
+        string sn = "";
+        if(not_overlap(arr[i], s))
+        {
+            sn = s + arr[i];
+        }
+        
+        return max(bt(arr, i+1, s), bt(arr, i+1, sn));
+    }
+    int maxLength(vector<string>& arr) 
+    {
+        return bt(arr, 0, "");
+        
+    }
+};
