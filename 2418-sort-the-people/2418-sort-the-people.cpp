f0527a6f26b6d162fd87fdd31dad095d800f9e0d
Nowfel Mashnoor <nmmashnoor@gmail.com>
2024-07-23 01:00:29 +0600

    Time: 33 ms (40.77%), Space: 26 MB (70.36%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.cpp
+++ 2418-sort-the-people/2418-sort-the-people.cpp
@@ -2,18 +2,21 @@ class Solution {
 public:
     vector<string> sortPeople(vector<string>& names, vector<int>& heights) 
     {
-        for(int i = 0; i<heights.size(); i++)
-        {
-            for(int j = i+1; j<heights.size(); j++)
-            {
-                if(heights[i] < heights[j])
-                {
-                    swap(heights[i], heights[j]);
-                    swap(names[i], names[j]);
-                }
-            }
+         vector<pair<int, string>> heightNamePairs;
+        for (int i = 0; i < heights.size(); ++i) {
+            heightNamePairs.emplace_back(heights[i], names[i]);
         }
-        
+
+        // Sort the pairs in descending order based on heights
+        std::sort(heightNamePairs.begin(), heightNamePairs.end(), [](const pair<int, string>& a, const pair<int, string>& b) {
+            return a.first > b.first; // Sort by height in descending order
+        });
+
+        // Extract the sorted names
+        for (int i = 0; i < heightNamePairs.size(); ++i) {
+            names[i] = heightNamePairs[i].second;
+        }
+
         return names;
         
     }
