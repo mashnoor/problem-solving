25003dc3c6079b98b27cdfc2ae0c1533f3dbbcb3
Nowfel Mashnoor <nmmashnoor@gmail.com>
2024-07-24 08:17:28 +0600

    Time: 179 ms (80.09%), Space: 88.7 MB (59.74%) - LeetHub

---
+++ 2191-sort-the-jumbled-numbers/2191-sort-the-jumbled-numbers.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<int> sortJumbled(vector<int>& mapping, vector<int>& nums) 
+    {
+        vector<pair<int, int>> newmp;
+        for(int i = 0; i<nums.size(); i++)
+        {
+            int num = nums[i];
+            string val = to_string(num);
+            int ival = 0;
+            for(auto c : val)
+            {
+                int ic = c - '0';
+                ival *= 10;
+                ival += mapping[ic];
+            }
+            newmp.emplace_back(ival, i);
+        }
+        
+        sort(newmp.begin(), newmp.end(), [](const pair<int,int>& a, const pair<int,int>& b){
+            if(a.first == b.first)
+            {
+                return a.second < b.second;
+            }
+            
+            return a.first < b.first;
+            
+        });
+        vector<int> result;
+        for(int i = 0; i<newmp.size(); i++)
+        {
+            result.push_back(nums[newmp[i].second]);
+        }
+        
+        return result;
+        
+    }
+};
