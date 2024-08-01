5627f22bd1f4515aab8f5a0f8f7d58c027c872d2
Nowfel Mashnoor <nmmashnoor@gmail.com>
2024-07-31 17:49:29 -0700

    Time: 13 ms (5.72%), Space: 17.6 MB (16.71%) - LeetHub

--- 2678-number-of-senior-citizens/2678-number-of-senior-citizens.cpp
+++ 2678-number-of-senior-citizens/2678-number-of-senior-citizens.cpp
@@ -5,8 +5,10 @@ public:
         int cnt = 0;
         for(string person : details)
         {
-            if(person[11]>'6') cnt++;
-            if(person[11] == '6' && person[12] > '0') cnt++;
+            if(person[11] > '6' || (person[11] == '6' && person[12] > '0')) 
+            {
+                cnt++;
+            }
         }
         
         return cnt;
